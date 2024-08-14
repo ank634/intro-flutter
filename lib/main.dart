7f98080a8dfe6d426a1a817e65efd44b6ef34041
Emmanuel Bastidas <emmanuel.bastidas@gmail.com>
2024-08-14 00:40:36 -0700

    removed unecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:english_words/english_words.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'login_form.dart';
 import 'login_page.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -15,21 +13,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: LoginPage()
-    );
+    return const MaterialApp(home: LoginPage());
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
