c272886d2ece4afaa40ff2d558e635f5de2c92d0
Emmanuel Bastidas <emmanuel.bastidas@gmail.com>
2024-08-14 00:39:28 -0700

    added support for persist cookies to store session id-s

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -1,21 +1,21 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:dio/dio.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:path_provider/path_provider.dart';
 import 'logged_in_page.dart';
 
-
-class LoginForm extends StatefulWidget{
+class LoginForm extends StatefulWidget {
   final String loginApiUrl;
   const LoginForm(this.loginApiUrl, {super.key});
-  
+
   @override
   State<StatefulWidget> createState() => _LoginFormState();
-
 }
 
-class _LoginFormState extends State<LoginForm>{
+class _LoginFormState extends State<LoginForm> {
   final usernameTextFieldController = TextEditingController();
   final passwordTextFieldController = TextEditingController();
 
@@ -27,42 +27,61 @@ class _LoginFormState extends State<LoginForm>{
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,  // Center vertically multiple widgets
-      crossAxisAlignment: CrossAxisAlignment.center,  // Center horizontally multiple widgets
+        mainAxisAlignment:
+            MainAxisAlignment.center, // Center vertically multiple widgets
+        crossAxisAlignment:
+            CrossAxisAlignment.center, // Center horizontally multiple widgets
 
-      children: [TextField(decoration: InputDecoration(labelText: 'Username'), controller: usernameTextFieldController,),
-                TextField(obscureText: true, decoration: InputDecoration(labelText: 'Password'), controller: passwordTextFieldController),
-                ElevatedButton(onPressed: (){
-                  login().then((response) {
-                    if (response.statusCode == 200){
-                      Navigator.pop(context);
-                      print(response.headers);
-                      Navigator.push(
-                        context, 
-                        MaterialPageRoute(builder: (context) => const LoggedInPage()));
-                    }
-                  });
-                }, child: Text('Login'))]
-      );
-    
+        children: [
+          TextField(
+            decoration: const InputDecoration(labelText: 'Username'),
+            controller: usernameTextFieldController,
+          ),
+          TextField(
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+              controller: passwordTextFieldController),
+          ElevatedButton(
+              onPressed: () {
+                login().then((response) {
+                  if (response.statusCode == 200 && context.mounted) {
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoggedInPage()));
+                  }
+                });
+              },
+              child: const Text('Login'))
+        ]);
   }
 
-  Future<http.Response> login() async{
+  Future<Response> login() async {
+    final dio = await _networkingSetup();
     String username = usernameTextFieldController.text;
     String password = passwordTextFieldController.text;
+    const String url = 'http://127.0.0.1:5000/auth/login';
+    const Map<String, String> headers = {'Content-Type': 'application/json'};
+    final String body =
+        jsonEncode({'username': username, 'password': password});
 
-    final response = await http.post(Uri.parse('http://127.0.0.1:5000/auth/login'),
-    headers: <String, String>{
-      'Content-Type': 'application/json'
-    },
-    body: jsonEncode(<String, String>{
-      'username': username,
-      'password': password
-    })
-    );
-
+    Response response =
+        await dio.post(url, data: body, options: Options(headers: headers));
     return response;
+  }
+
+  Future<Dio> _networkingSetup() async {
+    final dio = Dio();
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String appDocPath = appDocDir.path;
+    final cookieJar = PersistCookieJar(
+        ignoreExpires: true, storage: FileStorage('$appDocPath/.cookies/'));
+
+    dio.interceptors.add(CookieManager(cookieJar));
+
+    return dio;
   }
 }
