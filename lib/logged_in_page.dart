004e1e48bcde8b0e4b7d5bf8a2a07dff2c92ec55
Emmanuel Bastidas <emmanuel.bastidas@gmail.com>
2024-08-14 00:38:43 -0700

    added support to logout

--- lib/logged_in_page.dart
+++ lib/logged_in_page.dart
@@ -1,39 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:namer_app/login_page.dart';
+import 'dart:io';
 
-class LoggedInPage extends StatelessWidget{
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:namer_app/login_page.dart';
+import 'package:path_provider/path_provider.dart';
+
+class LoggedInPage extends StatelessWidget {
   const LoggedInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('You Are Logged In'),
-      ),
-      body: ElevatedButton(onPressed: (){
-        logout().then((response) {
-          
-          if (response.statusCode == 200){
-            
-            Navigator.pop(context);
-
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => const LoginPage()));
-          }
-        });
-      }, child: Text('Log out'))
-    );
+        appBar: AppBar(
+          title: const Text('You Are Logged In'),
+        ),
+        body: ElevatedButton(
+            onPressed: () {
+              logout().then((response) {
+                if (response.statusCode == 200 && context.mounted) {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                }
+              });
+            },
+            child: const Text('Log out')));
   }
 
-  // TODO: find out how to store cookie persistent 
-  Future<http.Response> logout() async{
-    final response = await http.delete(Uri.parse('http://127.0.0.1:5000/auth/logout'),
-    headers: <String, String>{
-    'Content-Type': 'application/json',
-    'Cookie': 'session-id=a58b7239-032a-461a-bf34-7c50c2c74d69'
-    });
+  Future<Response> logout() async {
+    final dio = await _networkingSetup();
+    const String url = 'http://127.0.0.1:5000/auth/logout';
+    Map<String, String> headers = {'Content-Type': 'application/json'};
+    Response response =
+        await dio.delete(url, options: Options(headers: headers));
 
     return response;
+  }
+
+  Future<Dio> _networkingSetup() async {
+    final dio = Dio();
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String appDocPath = appDocDir.path;
+    final cookieJar = PersistCookieJar(
+        ignoreExpires: true, storage: FileStorage('$appDocPath/.cookies/'));
+
+    dio.interceptors.add(CookieManager(cookieJar));
+
+    return dio;
   }
 }
