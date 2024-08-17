b0fb18cddc9187e73ea6ddf023c69c1f66e5000c
Emmanuel Bastidas <emmanuel.bastidas@gmail.com>
2024-08-16 23:48:53 -0700

    removed unecessary dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'login_form.dart';
-import 'login_page.dart';
+import 'ui/login_page.dart';
 
 void main() {
   runApp(MyApp());
